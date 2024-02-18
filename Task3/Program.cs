0afe490f15f600a3bb1c375ea9197032ed522f4e
Roman <dmot.boss@mail.ru>
2024-02-18 21:49:12 +0300

    add lesson4 task3

---
+++ Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿string GetLetters(string s)
+{
+    string letters = "";
+    foreach(char w in s)
+    {
+        if(char.IsAsciiLetter(w) == true)
+        {
+            letters += w;
+        }
+    }
+    return letters;
+}
+
+string str = Console.ReadLine();
+string result = GetLetters(str);
+Console.WriteLine(result);
+        
+        
+
+        
+        
+
